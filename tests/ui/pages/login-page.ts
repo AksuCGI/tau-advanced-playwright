eba27563d30383db0b896eb830d577930dab350c
Aleksandra Löytäinen <aleksandra.loytainen@cgi.com>
2026-09-25 11:24:23 +0300

    Deprecated some packages due to playwright test run errors. Updated title check

--- tests/ui/pages/login-page.ts
+++ tests/ui/pages/login-page.ts
@@ -1,4 +1,4 @@
-import { type Page, type Locator , expect } from '@playwright/test';
+import { type Page, type Locator, expect } from '@playwright/test';
 import messages from '../../utils/messages';
 
 class LoginPage {
@@ -32,7 +32,7 @@ class LoginPage {
 
   async checkLoggedIn() {
     await expect(this.page).toHaveURL(/.*profile/);
-    await expect(this.page).toHaveTitle(/DEMOQA/);
+    await expect(this.page).toHaveTitle(/demosite/);
   }
 
   async checkInvalidCredentials() {
